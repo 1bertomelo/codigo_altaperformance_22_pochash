b970d059b7c96eac4b00a715f4a9162b2ca452a9
betomelo_9 <betomelo_9@hotmail.com>
2022-10-17 18:03:34 -0300

    primeira versao poc

---
+++ PocHash/PocHash/HashExemplo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PocHash
+{
+	public class HashExemplo : IHash<int>
+	{
+		private int?[] Vetor { get; set; }
+		public int N { get; private set; }
+
+		public int QtdeInserida { get; private set; }
+
+		public HashExemplo(int tamanho)
+		{
+			Vetor = new int?[tamanho];
+			N = tamanho;
+			QtdeInserida = 0;
+
+		}
+
+		public int Busca(int item)
+		{
+			return 1;
+		}
+
+		public void Inserir(int item)
+		{
+			int posicao = FuncoHashModular(item);
+			if(N == QtdeInserida)
+				throw new NotImplementedException("Não é possível inserir, vetor sem espaço");
+			else
+			{
+				int i = 0;
+				bool inseriu=false;
+
+				while (!inseriu)
+				{
+					posicao = FuncoHashModular(item+i);
+					if (PosicaoVazia(posicao))
+					{
+						Vetor[posicao] = item;
+						inseriu = true;
+						QtdeInserida++;
+					}
+					else
+					{
+						i++;
+					}
+
+				}
+			}
+		}
+
+		private int FuncoHashModular(int x)
+		{
+			return x % N;
+		}
+
+		private bool PosicaoVazia(int posicao) => Vetor[posicao] == null ? true : false;
+
+
+	}
+}
---
+++ PocHash/PocHash/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PocHash;
+
+Console.WriteLine("Inicio!");
+HashExemplo h = new HashExemplo(13);
+h.Inserir(26);
+h.Inserir(40);
+h.Inserir(52);
+h.Inserir(19);
+Console.WriteLine("Fim");
+
+
